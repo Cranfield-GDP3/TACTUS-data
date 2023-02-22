4f7b68dd6f5fddb61272549e739d588c61d2ab6c
RuochenYin <122464745+RuochenYin@users.noreply.github.com>
2023-02-22 14:44:01 +0000

    doc: convert to numpy docstring conventions
    In order to have an uniform docstring experience on the project,
    the numpy conventions are used.

--- src/utils/video_to_img.py
+++ src/utils/video_to_img.py
@@ -2,18 +2,28 @@ import cv2
 from pathlib import Path
 
 
-def extract_frames(video_path: Path, result_path: Path, desired_fps: int):
-    '''
-        :param video_path: video path
-        :param result_path: the location where the frame image is stored
-        :param desired_fps
-    '''
+def extract_frames(
+        video_path: Path,
+        result_path: Path,
+        desired_fps: int):
+    """Return the series of frames that convert from a
+    video source.
+
+    Parameters
+    ----------
+    video_path : Path
+        The video path that gives the video source
+    result_path : Path
+        The location where the frame image is stored
+    desired_fps : int
+        The fps we want to have
+    """
 
     cap = cv2.VideoCapture(str(video_path))
     fps = cap.get(cv2.CAP_PROP_FPS)
     extract_frequency = int(fps / desired_fps)
     n_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    n = len(str(n_frame))
+    n_frame_len = len(str(n_frame))
 
     count = 1
     while True:
@@ -21,7 +31,8 @@ def extract_frames(video_path: Path, result_path: Path, desired_fps: int):
         if frame is None:
             break
         if count % extract_frequency == 0:
-            save_path = result_path / f"{str(count).zfill(n)}.jpg"
+            save_path = result_path / f"{str(count).zfill(n_frame_len)}.jpg"
             cv2.imwrite(save_path, frame)
         count += 1
+
     cap.release()
