a38fb58d3cc5b25ca17e58f150023ba2f52feeb3
Marc Bresson <marco.bresson@gmail.com>
2023-02-23 16:30:48 +0000

    feat: add frame extraction for ut interaction

--- tactus_data/datasets/ut_interaction.py
+++ tactus_data/datasets/ut_interaction.py
@@ -9,10 +9,14 @@ import shutil
 
 import requests
 from tactus_data.utils.read_dataset_urls import read_dataset_urls
+from tactus_data.utils.interim_name_convention import interim_name_convention
+from tactus_data.utils.video_to_img import extract_frames
 
 
 class UTInteraction:
-    DEFAULT_RAW_DIR = Path("data/raw/ut_interaction")
+    NAME = "ut_interaction"
+    DEFAULT_RAW_DIR = Path(f"data/raw/{NAME}")
+    DEFAULT_INTERIM_DIR = Path("data/interim/")
 
     def download(self, download_dir: Path = DEFAULT_RAW_DIR):
         """
@@ -35,8 +39,6 @@ class UTInteraction:
         shutil.rmtree(download_dir / "segmented_set1")
         shutil.rmtree(download_dir / "segmented_set2")
 
-        self.create_labels(download_dir)
-
     def move_videos(self, download_dir: Path):
         """
         The archive contains two subfolders and this function move every
@@ -44,32 +46,80 @@ class UTInteraction:
 
         Parameters
         ----------
-        download_dir : Path, optional
-            The path where the data have been downloaded, by default DEFAULT_RAW_DIR
+        download_dir : Path
+            The path where the data have been downloaded
         """
 
         for video_path in download_dir.glob("*/*.avi"):
             video_path.rename(download_dir / video_path.name)
 
-    ACTION_INDEXES = ["hand shaking", "hugging", "kicking",
-                      "pointing", "punching", "pushing"]
-
-
-    def create_labels(self, download_dir: Path):
+    def extract_frames(
+            self,
+            download_dir: Path = DEFAULT_RAW_DIR,
+            output_dir: Path = DEFAULT_INTERIM_DIR,
+            desired_fps: int = 10
+        ):
         """
-        generate a label file from the name of the video which contains:
-        a sample number, a sequence number, and the action number.
+        Extract frame for this dataset and label them by
+        moving them into different subfolders.
 
         Parameters
         ----------
         download_dir : Path, optional
-            The path where the data have been downloaded, by default DEFAULT_RAW_DIR
+            The path where the data have been downloaded,
+            by default DEFAULT_RAW_DIR
+        output_dir : Path, optional
+            The path to where to save the frames,
+            by default DEFAULT_INTERIM_DIR
+        desired_fps : int, optional
+            The fps we want to have, by default 10
         """
-
         for video_path in download_dir.glob("*.avi"):
-            _, _, action = video_path.stem.split("_")
-            corresponding_action = self.ACTION_INDEXES[int(action)]
+            label = self.label_from_path(video_path)
+            uid = self.uid_from_path(video_path)
 
-            label_path = download_dir / (video_path.stem + ".txt")
-            with label_path.open("w", encoding="utf8") as label_file:
-                label_file.write(corresponding_action)
+            frame_output_dir = (output_dir
+                                / label
+                                / interim_name_convention(self.NAME, uid, desired_fps))
+            extract_frames(video_path, frame_output_dir, desired_fps)
+
+    ACTION_INDEXES = ["neutral", "neutral", "kicking",
+                      "neutral", "punching", "pushing"]
+
+    def label_from_path(self, video_path: Path) -> str:
+        """
+        Extract the label from the video name.
+
+        Parameters
+        ----------
+        video_path : Path
+            The path of the video to extract a label from.
+
+        Returns
+        -------
+        str :
+            the corresponding label
+        """
+        _, _, action = video_path.stem.split("_")
+        label = self.ACTION_INDEXES[int(action)]
+
+        return label
+
+    def uid_from_path(self, video_path: Path) -> str:
+        """
+        Compute an unique id from the path.
+
+        Parameters
+        ----------
+        video_path : Path
+            The path of the video to extract a label from.
+
+        Returns
+        -------
+        str :
+            unique id for the video in the dataset
+        """
+        sample_number, sequence_number, _ = video_path.stem.split("_")
+        unique_id = f"{sample_number.zfill(2)}_{sequence_number}"
+
+        return unique_id
