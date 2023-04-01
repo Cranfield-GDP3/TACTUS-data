cc712a8fdc57941ed7482261431adf3315ad6774
Marc Bresson <marco.bresson@gmail.com>
2023-04-01 13:56:04 +0100

    refactor: remove duplicate delete_conf_kp function

--- tactus_data/utils/skeletonization.py
+++ tactus_data/utils/skeletonization.py
@@ -38,17 +38,6 @@ def yolov7(input_dir: Path, model: Yolov7):
     return formatted_json
 
 
-def delete_confidence_kpt(skeleton: list) -> list:
-    """delete the confidence for each keypoint of a skeleton"""
-    return drop_every_nth_index(skeleton, 3)
-
-
-def drop_every_nth_index(initial_list: list, n: int) -> list:
-    """remove every nth index from a list"""
-    del initial_list[n-1::n]
-    return initial_list
-
-
 def round_values(skeleton: list) -> list:
     """round all the values of a list. Useful to save a lot of space
     when saving the skeletons to a file"""
